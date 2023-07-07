0021d6bdea269694df910330e748e2b4fef9fbc6
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-07 19:06:03 +0545

    Connected to MongoDB

--- backend/Controllers/goalController.js
+++ backend/Controllers/goalController.js
@@ -4,32 +4,32 @@ const asyncHandler = require('express-async-handler')
 //@route GET /api/goals
 //@access Private after we add authentication
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get Goals'})
+    res.status(200).json({ message: 'Get Goals' })
 
 })
 //@desc Set goals
 //@route POST /api/goals
 //@access Private after we add authentication
-const setGoals = asyncHandler (async(req, res) => {
-    if(!req.body.text) {
+const setGoals = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
         res.status(400)
-        throw new Error('Please add a text field') 
+        throw new Error('Please add a text field')
 
     }
-    res.status(200).json({message: 'Set Goals'})
-}) 
+    res.status(200).json({ message: 'Set Goals' })
+})
 //@desc Update goals
 //@route PUT /api/goals/:id
 //@access Private after we add authentication
-const updateGoals = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}`})
+const updateGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}` })
 
 })
 //@desc Delete goals
 //@route DELETE /api/goals/:id
 //@access Private after we add authentication
-const deleteGoals = asyncHandler( async(req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}`})
+const deleteGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}` })
 
 })
 
