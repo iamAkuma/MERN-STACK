9bc6ddc3b11dbf8af992414f5f7355ad836c9196
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-07 18:14:04 +0545

    MERN

--- backend/Controllers/goalController.js
+++ backend/Controllers/goalController.js
@@ -9,9 +9,12 @@ const getGoals = (req, res) => {
 //@route POST /api/goals
 //@access Private after we add authentication
 const setGoals = (req, res) => {
-    res.status(200).json({ message: 'Set Goals'})
+    if(!req.body.text) {
+        res.status(400).json({ message: 'Please add a text field'})
 
-}
+    }
+    res.status(200).json({message: 'Set Goals'})
+} 
 //@desc Update goals
 //@route PUT /api/goals/:id
 //@access Private after we add authentication
