98a209c4fdb1bb5480e644fc62eb4670daa64e1a
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-13 18:18:18 +0545

    Goals are now showing in Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import {useNavigate} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
 import { getGoals } from "../features/goals/goalSlice"
 import { reset} from "../features/goals/goalSlice"
@@ -46,6 +47,16 @@ if(isLoading) {
 
       
       <GoalForm/>
+
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal = {goal}/>
+            ))}
+          </div>
+        ) : (<h3> You have not set any goals </h3>)}
+      </section>
     </>
   )
 }
