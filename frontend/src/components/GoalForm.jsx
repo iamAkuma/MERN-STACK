34d8ca6951d7af7172bae87a4853af1a27b5fd32
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-13 19:07:20 +0545

    GoalSetter App Completed

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,32 +1,39 @@
-import { useState } from "react"
-import { useDispatch } from "react-redux"
-import {createGoal} from '../features/goals/goalSlice'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createGoal } from '../features/goals/goalSlice'
 
 function GoalForm() {
-    const [text, setText] = useState('')
+  const [text, setText] = useState('')
 
-    const dispatch = useDispatch()
-    const onSubmit = e =>{
-        e.preventDefault()
+  const dispatch = useDispatch()
 
-        dispatch(createGoal({text}))
-        setText('')
-    }
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    dispatch(createGoal({ text }))
+    setText('')
+  }
+
   return (
-    <>
-    <section className="form">
-        <form onSubmit={onSubmit}>
-        <div className="form-group">
-            <label htmlFor="text">Goal</label>
-            <input type="text" id='text' value={text} onChange={(e) => setText(e.target.value)}/>
+    <section className='form'>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='text'>Goal</label>
+          <input
+            type='text'
+            name='text'
+            id='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
         </div>
-        <div className="form-group">
-            <button className="btn btn-block">Submit</button>
+        <div className='form-group'>
+          <button className='btn btn-block' type='submit'>
+            Add Goal
+          </button>
         </div>
-        </form>
+      </form>
     </section>
-      <h1>Goal Form</h1>
-    </>
   )
 }
 
